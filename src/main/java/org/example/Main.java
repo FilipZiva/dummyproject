d0aed07ec3664f5c22cbbbc13c594471cf72294b
Filip Zivanovic <filip.zivanovic@stud.fh-campuswien.ac.at>
2023-11-18 15:03:42 +0100

    commiting dummyproject

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.Model.Persistance.StatusDao;
+import org.example.Model.Service.StatusService;
+import org.example.Model.Util.HibernateConnection;
+import org.hibernate.SessionFactory;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static StatusService statusService;
+
+
+    public static void main(String[] args) {
+        SessionFactory connection = HibernateConnection.getSessionFactory();
+        StatusDao statusDao = new StatusDao(connection);
+        statusService = new StatusService(statusDao);
+        InitClasses();
+        logger.log(Level.ERROR, statusService.getAllStatuses());
+    }
+
+    public static void InitClasses() {
+        statusService.initStatus();
+    }
+}
---
+++ src/main/java/org/example/Model/Service/StatusService.java
@@ -0,0 +1,38 @@
+package org.example.Model.Service;
+
+import org.example.Model.Domain.Status;
+import org.example.Model.Persistance.StatusDao;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+public class StatusService {
+
+    private StatusDao statusDao;
+
+    public StatusService(StatusDao statusDao) {
+        this.statusDao = statusDao;
+    }
+
+    public Status getStatus(Long id) {
+        return statusDao.findById(id);
+    }
+
+    public List<Status> getAllStatuses() {
+        return statusDao.findAll();
+    }
+
+    public void createStatus(Status status) {
+        statusDao.saveOrUpdate(status);
+    }
+
+    @PostConstruct
+    public void initStatus(){
+        Status status = new Status();
+        status.setName("High");
+        createStatus(status);
+
+    }
+
+}
+
